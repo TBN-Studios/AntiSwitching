17a0abba90c1897174ac707804d9e1d6a03d7d3a
TBN <casualmineri6@gmail.com>
2025-01-03 18:50:44 +0000

    AntiSwitching Plugin

---
+++ src/TBN/AntiSwitching/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TBN\AntiSwitching;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+class Main extends PluginBase implements Listener {
+    private bool $allowSwitching = false;
+
+    protected function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->allowSwitching = $this->getConfig()->get("allow-switching", false);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    /**
+     * @param EntityDamageByEntityEvent $event
+     * @priority HIGHEST
+     */
+    public function handleNoSwitch(EntityDamageByEntityEvent $event): void {
+        if (!$this->allowSwitching && $event->getModifier(EntityDamageEvent::MODIFIER_PREVIOUS_DAMAGE_COOLDOWN) < 0) {
+            $event->cancel();
+        }
+    }
+} 
